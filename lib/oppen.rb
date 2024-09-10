0c20b774b6b73899f80b6167dbb8d6ee8070c6b3
Amine Mike <amine.el-maalouf@epita.fr>
2024-09-10 16:25:09 +0200

    oppen: first working version

--- lib/oppen.rb
+++ lib/oppen.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 require_relative 'oppen/version'
+require_relative 'oppen/pretty_printer'
 
-# Oppen
+# Oppen.
 module Oppen
   module_function
 
-  # Hello world
-  def hello_world
-    puts 'Hello World!'
+  # Entry point of the pretty printer.
+  #
+  # @param tokens [Array[Token]] the list of tokens to be printed
+  # @param line_width [Integer] maximum line width desired
+  #
+  # @return [StringIO] output of the pretty printer
+  def pretty_print_tokens(tokens = [], line_width = 80)
+    pretty_printer = PrettyPrinter.new line_width
+    tokens.each do |token|
+      pretty_printer.pretty_print token
+    end
+    pretty_printer.output
   end
 end
