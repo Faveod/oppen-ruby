52e02a23b84089ec60aba42bca2bebf4be6a4520
Amine Mike <amine.el-maalouf@epita.fr>
2024-09-10 10:10:27 +0200

    token: implement token class

---
+++ lib/oppen/token.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+# Oppen
+module Oppen
+  # Token used in the oppen algorithm.
+  module Token
+    module BreakType
+      FITS = 0
+      INCONSISTENT = 1
+      CONSISTENT = 2
+    end
+
+    # String Token.
+    class String
+      attr_reader :value
+
+      def initialize(value)
+        @value = value
+      end
+
+      def length
+        value.length
+      end
+    end
+
+    # Break Token.
+    class Break
+      attr_reader :blank_space
+      attr_reader :offset
+
+      def initialize(blank_space = 1, offset = 0)
+        @blank_space = blank_space
+        @offset = offset
+      end
+    end
+
+    # Begin Token.
+    class Begin
+      attr_reader :break_type
+      attr_reader :offset
+
+      def initialize(break_type = BreakType::INCONSISTENT, offset = 2)
+        @offset = offset
+        @break_type = break_type
+      end
+    end
+
+    # End Token
+    class End
+      nil
+    end
+
+    # EOF Token
+    class EOF
+      nil
+    end
+  end
+end
