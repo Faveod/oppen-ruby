ed9fc7f3efc46921166e37b1b65f84e6e9ba3d07
Amine Mike <amine.el-maalouf@epita.fr>
2024-09-10 16:25:09 +0200

    oppen: first working version

--- bin/main.rb
+++ bin/main.rb
@@ -3,5 +3,21 @@
 
 # require 'oppen'
 require_relative '../lib/oppen'
+require_relative '../lib/oppen/token'
 
-Oppen.hello_world
+list = []
+
+list.append(Oppen::Token::Begin.new)
+list.append(Oppen::Token::String.new('XXXXXXXXXX'))
+list.append(Oppen::Token::Break.new)
+list.append(Oppen::Token::String.new('+'))
+list.append(Oppen::Token::Break.new)
+list.append(Oppen::Token::String.new('YYYYYYYYYY'))
+list.append(Oppen::Token::Break.new)
+list.append(Oppen::Token::String.new('+'))
+list.append(Oppen::Token::Break.new)
+list.append(Oppen::Token::String.new('ZZZZZZZZZZ'))
+list.append(Oppen::Token::End.new)
+list.append(Oppen::Token::EOF.new)
+
+puts Oppen.pretty_print_tokens list, 25
