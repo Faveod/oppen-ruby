6bd792f4a442f26903f81d89b1245a2f0b3132e7
Firas al-Khalil <firas.alkhalil@faveod.com>
2024-09-12 10:29:11 +0200

    test: lib: load oppen automatically
    And avoid manual loading of different parts of the lib.

--- test/lib.rb
+++ test/lib.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+lib = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib) if !$LOAD_PATH.include?(lib)
+
 require 'bundler/setup'
 # require 'debug'
 require 'minitest/autorun'
