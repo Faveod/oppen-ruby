57554d354c2b7cac2e747463fc0325ebd443a255
Firas al-Khalil <firas.alkhalil@faveod.com>
2024-09-12 17:04:57 +0200

    pretty-printer: use attr_reader

--- lib/oppen/pretty_printer.rb
+++ lib/oppen/pretty_printer.rb
@@ -10,6 +10,44 @@ module Oppen
   # PrettyPrinter class responsible for scanning tokens and passing
   # them to the printer.
   class PrettyPrinter
+    # Ring buffer left indexe.
+    #
+    # @note Called left as well in the original paper.
+    attr_reader :left
+
+    # Total number of spaces needed to print from start of buffer to the left.
+    #
+    # @note Called leftTotal as well in the original paper.
+    attr_reader :left_total
+
+    # @note Called printStack as well in the original paper.
+    attr_reader :print_stack
+
+    # Ring buffer right indexe.
+    #
+    # @note Called right as well in the original paper.
+    attr_reader :right
+
+    # Total number of spaces needed to print from start of buffer to the right.
+    #
+    # @note Called leftTotal as well in the original paper.
+    attr_reader :right_total
+
+    # Potential breaking positions.
+    #
+    # @note Called scanStack as well in the original paper.
+    attr_reader :scan_stack
+
+    # Size buffer, initially filled with nil.
+    #
+    # @note Called size as well in the orginal paper.
+    attr_reader :size
+
+    # Token buffer, initially filled with nil.
+    #
+    # @note Called token in the orginal paper.
+    attr_reader :tokens
+
     # @note Called PrettyPrintInit in the original paper.
     #
     # @param line_width [Integer]  maximum line width desired.
@@ -18,38 +56,19 @@ module Oppen
       # Maximum size if the stacks
       n = 3 * line_width
 
-      # Token buffer, initially filled with nil.
-      #
-      # @note Called token in the orginal paper.
-      @tokens = Array.new n
-
-      # Size buffer, initially filled with nil.
-      #
-      # @note Called size as well in the orginal paper.
-      @size = Array.new n
-
-      # Potential breaking positions.
-      #
-      # @note Called scanStack as well in the original paper.
-      @scan_stack = ScanStack.new n
-
-      # Total number of spaces needed to print from start of buffer to left/right.
-      #
-      # @note Called leftTotal and rightTotl as well in the original paper.
-      @left_total = @right_total = 1
-
-      # Ring buffer left and right indexes.
-      #
-      # @note Called left and right as well in the original paper.
-      @left = @right = 0
-
-      # @note Called printStack as well in the original paper.
+      @left = 0
+      @left_total = 1
       @print_stack = PrintStack.new line_width, line_delimiter
+      @right = 0
+      @right_total = 1
+      @scan_stack = ScanStack.new n
+      @size = Array.new n
+      @tokens = Array.new n
     end
 
     # @return [StringIO]
     def output
-      @print_stack.output
+      print_stack.output
     end
 
     # Core function of the algorithm responsible for populating the scan and print stack.
@@ -80,11 +99,11 @@ module Oppen
     #
     # @see Token::EOF
     def handle_eof
-      if !@scan_stack.empty?
+      if !scan_stack.empty?
         check_stack 0
-        advance_left @tokens[@left], @size[@left]
+        advance_left tokens[left], size[left]
       end
-      @print_stack.indent 0
+      print_stack.indent 0
     end
 
     # Handle Begin Token.
@@ -93,15 +112,17 @@ module Oppen
     #
     # @see Token::Begin
     def handle_begin(token)
-      if @scan_stack.empty?
-        @left_total = @right_total = 1
-        @left = @right = 0
+      if scan_stack.empty?
+        @left = 0
+        @left_total = 1
+        @right = 0
+        @right_total = 1
       else
         advance_right
       end
-      @tokens[@right] = token
-      @size[@right] = -@right_total
-      @scan_stack.push @right
+      tokens[right] = token
+      size[right] = -right_total
+      scan_stack.push right
     end
 
     # Handle End Token.
@@ -110,13 +131,13 @@ module Oppen
     #
     # @see Token::End
     def handle_end(token)
-      if @scan_stack.empty?
-        @print_stack.print_ token, 0
+      if scan_stack.empty?
+        print_stack.print_ token, 0
       else
         advance_right
-        @tokens[@right] = token
-        @size[@right] = -1
-        @scan_stack.push @right
+        tokens[right] = token
+        size[right] = -1
+        scan_stack.push right
       end
     end
 
@@ -126,16 +147,18 @@ module Oppen
     #
     # @see Token::Break
     def handle_break(token)
-      if @scan_stack.empty?
-        @left_total = @right_total = 1
-        @left = @right = 0
+      if scan_stack.empty?
+        @left = 0
+        @left_total = 1
+        @right = 0
+        @right_total = 1
       else
         advance_right
       end
       check_stack 0
-      @scan_stack.push @right
-      @tokens[@right] = token
-      @size[@right] = -@right_total
+      scan_stack.push right
+      tokens[right] = token
+      size[right] = -right_total
       @right_total += token.blank_space
     end
 
@@ -145,12 +168,12 @@ module Oppen
     #
     # @see Token::String
     def handle_string(token)
-      if @scan_stack.empty?
-        @print_stack.print_ token, token.length
+      if scan_stack.empty?
+        print_stack.print_ token, token.length
       else
         advance_right
-        @tokens[@right] = token
-        @size[@right] = token.length
+        tokens[right] = token
+        size[right] = token.length
         @right_total += token.length
         check_stream
       end
@@ -162,13 +185,13 @@ module Oppen
     #
     # @return [Nil]
     def check_stream
-      return if @right_total - @left_total <= @print_stack.space
+      return if right_total - left_total <= print_stack.space
 
-      if !@scan_stack.empty? && @left == @scan_stack.bottom
-        @size[@scan_stack.pop_bottom] = Float::INFINITY
+      if !scan_stack.empty? && left == scan_stack.bottom
+        size[scan_stack.pop_bottom] = Float::INFINITY
       end
-      advance_left @tokens[@left], @size[@left]
-      return if @left == @right
+      advance_left tokens[left], size[left]
+      return if left == right
 
       check_stream
     end
@@ -179,8 +202,8 @@ module Oppen
     #
     # @return [Nil]
     def advance_right
-      @right = (@right + 1) % @scan_stack.length
-      return if @right != @left
+      @right = (right + 1) % scan_stack.length
+      return if right != left
 
       raise 'Token queue full'
     end
@@ -194,7 +217,7 @@ module Oppen
     def advance_left(token, token_length)
       return if token_length.negative?
 
-      @print_stack.print_ token, token_length
+      print_stack.print_ token, token_length
 
       case token
       when Token::Break
@@ -203,10 +226,10 @@ module Oppen
         @left_total += token_length
       end
 
-      return if @left == @right
+      return if left == right
 
-      @left = (@left + 1) % @scan_stack.length
-      advance_left @tokens[@left], @size[@left]
+      @left = (left + 1) % scan_stack.length
+      advance_left tokens[left], size[left]
     end
 
     # Updates the size buffer taking into
@@ -217,21 +240,21 @@ module Oppen
     # @param depth [Integer] depth of the group
     #
     # @return [Nil]
-    def check_stack(depth)
-      return if @scan_stack.empty?
+    def check_stack(depth) # rubocop:disable Metrics/AbcSize
+      return if scan_stack.empty?
 
-      x = @scan_stack.top
-      case @tokens[x]
+      x = scan_stack.top
+      case tokens[x]
       when Token::Begin
         if depth.positive?
-          @size[@scan_stack.pop] = @size[x] + @right_total
+          size[scan_stack.pop] = size[x] + right_total
           check_stack depth - 1
         end
       when Token::End
-        @size[@scan_stack.pop] = 1
+        size[scan_stack.pop] = 1
         check_stack depth + 1
       else
-        @size[@scan_stack.pop] = @size[x] + @right_total
+        size[scan_stack.pop] = size[x] + right_total
         if depth.positive?
           check_stack depth
         end
