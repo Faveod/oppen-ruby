bebc59445535e0c1f41fb948d9ae700c370c5d6f
Firas al-Khalil <firas.alkhalil@faveod.com>
2024-09-19 13:52:56 +0200

    bin: main: reduce to noop
    This should be a scratchpad for devs. We will keep the skeleton text in
    comments for now.
    
    I don't mind evolving the code in the comments if we need to.

--- bin/main.rb
+++ bin/main.rb
@@ -6,37 +6,37 @@ $LOAD_PATH.unshift(lib) if !$LOAD_PATH.include?(lib)
 
 require 'oppen'
 
-list = [
-  Oppen::Token::Begin.new,
-  Oppen::Token::String.new('XXXXXXXXXX'),
-  Oppen::Token::Break.new,
-  Oppen::Token::String.new('+'),
-  Oppen::Token::Break.new,
-  Oppen::Token::String.new('YYYYYYYYYY'),
-  Oppen::Token::Break.new,
-  Oppen::Token::String.new('+'),
-  Oppen::Token::Break.new,
-  Oppen::Token::String.new('ZZZZZZZZZZ'),
-  Oppen::Token::End.new,
-  Oppen::Token::EOF.new,
-]
+# list = [
+#   Oppen::Token::Begin.new,
+#   Oppen::Token::String.new('XXXXXXXXXX'),
+#   Oppen::Token::Break.new,
+#   Oppen::Token::String.new('+'),
+#   Oppen::Token::Break.new,
+#   Oppen::Token::String.new('YYYYYYYYYY'),
+#   Oppen::Token::Break.new,
+#   Oppen::Token::String.new('+'),
+#   Oppen::Token::Break.new,
+#   Oppen::Token::String.new('ZZZZZZZZZZ'),
+#   Oppen::Token::End.new,
+#   Oppen::Token::EOF.new,
+# ]
 
-puts Oppen.print tokens: list, margin: 25
+# puts Oppen.print tokens: list, margin: 25
 
-puts '--------------------------------------'
+# puts '--------------------------------------'
 
-wadler = Oppen::Wadler.new(margin: 25)
+# wadler = Oppen::Wadler.new(margin: 25)
 
-wadler.nest {
-  wadler.text 'XXXXXXXXXX'
-  wadler.breakable
-  wadler.text '+'
-  wadler.breakable
-  wadler.text 'YYYYYYYYYY'
-  wadler.breakable
-  wadler.text '+'
-  wadler.breakable
-  wadler.text 'ZZZZZZZZZZ'
-}
+# wadler.nest {
+#   wadler.text 'XXXXXXXXXX'
+#   wadler.breakable
+#   wadler.text '+'
+#   wadler.breakable
+#   wadler.text 'YYYYYYYYYY'
+#   wadler.breakable
+#   wadler.text '+'
+#   wadler.breakable
+#   wadler.text 'ZZZZZZZZZZ'
+# }
 
-puts wadler.output
+# puts wadler.output
