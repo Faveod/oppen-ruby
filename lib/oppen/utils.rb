796ce8e45e9a4ca7c906084828d8d70ffc6bbde5
Amine Mike <amine.el-maalouf@epita.fr>
2024-10-24 14:59:27 +0200

    wadler: add converter from tokens list to wadler

--- lib/oppen/utils.rb
+++ lib/oppen/utils.rb
@@ -15,5 +15,37 @@ module Oppen
       arr.fill(nil, size, 2 * size)
       [arr, 0, size]
     end
+
+    # Convert a list of tokens to its wadler representation.
+    # @param tokens [Array[Token]]
+    # @param base_indent [Integer]
+    #
+    # @return [String]
+    def self.tokens_to_wadler(tokens, base_indent = 4)
+      out = StringIO.new
+      def self.write(out, txt, nb_spaces) # rubocop:disable Lint/NestedMethodDefinition
+        out.write("#{' ' * nb_spaces}#{txt}\n")
+      end
+      nb_spaces = base_indent
+      tokens.each do |token|
+        case token
+        in Token::String
+          write(out, "out.text '#{token}'", nb_spaces)
+        in Token::LineBreak
+          write(out, 'out.break', nb_spaces)
+        in Token::Break
+          write(out, 'out.breakable', nb_spaces)
+        in Token::Begin
+          write(out, 'out.group {', nb_spaces)
+          nb_spaces += 2
+        in Token::End
+          nb_spaces -= 2
+          write(out, '}', nb_spaces)
+        in Token::EOF
+          write(out, '', nb_spaces) # new line
+        end
+      end
+      out.string
+    end
   end
 end
