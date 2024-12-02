d2d0294aff0e7322ab1bc314b0f7a55453402210
Amine Mike <amine.el-maalouf@epita.fr>
2024-12-02 16:47:37 +0100

    trim_trailing_whitespaces_test: refactor tests

--- test/trim_trailing_whitespaces_test.rb
+++ test/trim_trailing_whitespaces_test.rb
@@ -2,16 +2,6 @@
 
 require_relative 'lib'
 
-def trim_trailing_whitespaces_round_trip(builder_block, expected, whitespace)
-  printer = Oppen::Wadler.new(width: 5, whitespace:)
-  builder_block.call printer
-  _(printer.output).must_equal expected
-
-  printer_no_trailing = Oppen::Wadler.new(width: 5, whitespace:)
-  builder_block.call printer_no_trailing, false
-  _(printer_no_trailing.output).must_equal expected
-end
-
 describe 'Trim trailing whitespaces tests' do
   [
     ' ',
@@ -23,161 +13,173 @@ describe 'Trim trailing whitespaces tests' do
     '¬',
     'éÅ',
   ].each do |whitespace|
-    it "does not trim non trailing `#{whitespace}` whitespaces" do
-      printer = Oppen::Wadler.new(width: 5, whitespace:)
-      printer.text("#{whitespace}a#{whitespace}")
-      printer.text("#{whitespace}b")
-      _(printer.output).must_equal "#{whitespace}a#{whitespace}#{whitespace}b"
+    [
+      {
+        title: "does not trim non trailing `#{whitespace}` whitespaces",
+        block: proc { |printer|
+          printer.text("#{whitespace}a#{whitespace}")
+          printer.text("#{whitespace}b")
+        },
+        expected: "#{whitespace}a#{whitespace}#{whitespace}b",
+      },
+      {
+        title: "trims a pure `#{whitespace}` whitespace token",
+        block: proc { |printer|
+          printer.text(whitespace)
+          printer.break
+        },
+        expected: "\n",
+      },
+      {
+        title: "trims multiple pure `#{whitespace}` whitespace tokens",
+        block: proc { |printer|
+          printer.text(whitespace * 42)
+          printer.break
+        },
+        expected: "\n",
+      },
+    ].each do |test|
+      it test[:title] do
+        printer = Oppen::Wadler.new(width: 5, whitespace:)
+        test[:block].call printer
+        _(printer.output).must_equal test[:expected]
+      end
     end
 
-    it "trims a pure `#{whitespace}` whitespace token" do
-      printer = Oppen::Wadler.new(width: 5, whitespace:)
-      printer.text(whitespace)
-      printer.break
-      _(printer.output).must_equal "\n"
-    end
-
-    it "trims multiple pure `#{whitespace}` whitespace tokens" do
-      printer = Oppen::Wadler.new(width: 5, whitespace:)
-      printer.text(whitespace * 42)
-      printer.break
-      _(printer.output).must_equal "\n"
-    end
-
-    it "trims a single `#{whitespace}` trailing whitespace" do
-      builder_block = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace if add_trailing}")
-        out.break
-        out.text("#{whitespace}b")
-      }
-      expected = <<~LANG.chomp
-        #{whitespace}a
-        #{whitespace}b
-      LANG
-
-      trim_trailing_whitespaces_round_trip(builder_block, expected, whitespace)
-    end
-
-    it "trims multiple `#{whitespace}` trailing whitespaces" do
-      builder_block = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
-        out.break
-        out.text("#{whitespace}b")
-      }
-      expected = <<~LANG.chomp
-        #{whitespace}a
-        #{whitespace}b
-      LANG
-
-      trim_trailing_whitespaces_round_trip(builder_block, expected, whitespace)
-    end
-
-    it "trims `#{whitespace}` whitespace of previous parent group" do
-      builder_block = proc { |out, add_trailing = true|
-        out.text("a#{whitespace if add_trailing}")
-        out.group {
-          out.group {
-            out.break
-            out.text('b')
-          }
-        }
-      }
-      expected = <<~LANG.chomp
-        a
-        b
-      LANG
-
-      trim_trailing_whitespaces_round_trip(builder_block, expected, whitespace)
-    end
-
-    it "trims `#{whitespace}` whitespace of previous child group" do
-      builder_block = proc { |out, add_trailing = true|
-        out.group {
-          out.group {
-            out.group {
-              out.text("a#{whitespace if add_trailing}")
+    # Round trip tests.
+    [
+      {
+        title: "trims a single `#{whitespace}` trailing whitespace",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace if add_trailing}")
+          printer.break
+          printer.text("#{whitespace}b")
+        },
+        expected: <<~LANG.chomp,
+          #{whitespace}a
+          #{whitespace}b
+        LANG
+      },
+      {
+        title: "trims multiple `#{whitespace}` trailing whitespaces",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
+          printer.break
+          printer.text("#{whitespace}b")
+        },
+        expected: <<~LANG.chomp,
+          #{whitespace}a
+          #{whitespace}b
+        LANG
+      },
+      {
+        title: "trims `#{whitespace}` whitespace of previous parent group",
+        block: proc { |printer, add_trailing = true|
+          printer.text("a#{whitespace if add_trailing}")
+          printer.group {
+            printer.group {
+              printer.break
+              printer.text('b')
             }
           }
-          out.break
-          out.text('b')
-        }
-      }
-      expected = <<~LANG.chomp
-        a
-        b
-      LANG
+        },
+        expected: <<~LANG.chomp,
+          a
+          b
+        LANG
+      },
+      {
+        title: "trims `#{whitespace}` whitespace of previous child group",
+        block: proc { |printer, add_trailing = true|
+          printer.group {
+            printer.group {
+              printer.group {
+                printer.text("a#{whitespace if add_trailing}")
+              }
+            }
+            printer.break
+            printer.text('b')
+          }
+        },
+        expected: <<~LANG.chomp,
+          a
+          b
+        LANG
+      },
+      {
+        title: "trims `#{whitespace}` of pattern: text, whitespace, break",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace if add_trailing}")
+          printer.break
+        },
+        expected: "#{whitespace}a\n",
+      },
+      {
+        title: "trims `#{whitespace}` of pattern: text, whitespaces, break",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
+          printer.break
+        },
+        expected: "#{whitespace}a\n",
+      },
+      {
+        title: "trims `#{whitespace}` of pattern: text, whitespace, breaks",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace if add_trailing}")
+          printer.break
+          printer.break
+          printer.break
+          printer.break
+        },
+        expected: "#{whitespace}a\n\n\n\n",
+      },
+      {
+        title: "trims `#{whitespace}` of pattern: text, whitespaces, breaks",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
+          printer.break
+          printer.break
+          printer.break
+          printer.break
+        },
+        expected: "#{whitespace}a\n\n\n\n",
+      },
+      {
+        title: "does not trim `#{whitespace}` of pattern: text, whitespace, breaks, whitespaces",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
+          printer.break
+          printer.break
+          printer.text("#{whitespace}a#{whitespace * 42}")
+        },
+        expected: <<~LANG.chomp,
+          #{whitespace}a
 
-      trim_trailing_whitespaces_round_trip(builder_block, expected, whitespace)
-    end
+          #{whitespace}a#{whitespace * 42}
+        LANG
+      },
+      {
+        title: "trims `#{whitespace}` of pattern: text, whitespace, breaks, whitespaces, breaks",
+        block: proc { |printer, add_trailing = true|
+          printer.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
+          printer.break
+          printer.break
+          printer.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
+          printer.break
+          printer.break
+        },
+        expected: "#{whitespace}a\n\n#{whitespace}a\n\n",
+      },
+    ].each do |test|
+      it test[:title] do
+        printer = Oppen::Wadler.new(width: 5, whitespace:)
+        test[:block].call printer
+        _(printer.output).must_equal test[:expected]
 
-    it "trims `#{whitespace}` of pattern: text, whitespace(s), break" do
-      builder_block1 = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace if add_trailing}")
-        out.break
-      }
-      builder_block2 = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
-        out.break
-      }
-      expected = <<~LANG
-        #{whitespace}a
-      LANG
-
-      trim_trailing_whitespaces_round_trip(builder_block1, expected, whitespace)
-      trim_trailing_whitespaces_round_trip(builder_block2, expected, whitespace)
-    end
-
-    it "trims `#{whitespace}` of pattern: text, whitespace(s), breaks" do
-      builder_block1 = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace if add_trailing}")
-        out.break
-        out.break
-        out.break
-        out.break
-        out.break
-      }
-      builder_block2 = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
-        out.break
-        out.break
-        out.break
-        out.break
-        out.break
-      }
-      expected = "#{whitespace}a\n\n\n\n\n"
-
-      trim_trailing_whitespaces_round_trip(builder_block1, expected, whitespace)
-      trim_trailing_whitespaces_round_trip(builder_block2, expected, whitespace)
-    end
-
-    it "does not trim `#{whitespace}` of pattern: text, whitespace, breaks, whitespaces" do
-      builder_block = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
-        out.break
-        out.break
-        out.text("#{whitespace}a#{whitespace * 42}")
-      }
-      expected = <<~LANG.chomp
-        #{whitespace}a
-
-        #{whitespace}a#{whitespace * 42}
-      LANG
-
-      trim_trailing_whitespaces_round_trip(builder_block, expected, whitespace)
-    end
-
-    it "trims `#{whitespace}` of pattern: text, whitespace, breaks, whitespaces, breaks" do
-      builder_block = proc { |out, add_trailing = true|
-        out.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
-        out.break
-        out.break
-        out.text("#{whitespace}a#{whitespace * 42 if add_trailing}")
-        out.break
-        out.break
-      }
-      expected = "#{whitespace}a\n\n#{whitespace}a\n\n"
-
-      trim_trailing_whitespaces_round_trip(builder_block, expected, whitespace)
+        printer_no_trailing = Oppen::Wadler.new(width: 5, whitespace:)
+        test[:block].call printer_no_trailing, false
+        _(printer_no_trailing.output).must_equal test[:expected]
+      end
     end
   end
 end
